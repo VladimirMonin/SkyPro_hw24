5c7bab82b3b10e462fad558592366b40c1e58458
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-08-03 20:47:06 +0600

    feat: done!

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import os
 from flask import Flask, request, abort, jsonify
-
+from utils import get_query
 import logging
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
@@ -16,28 +16,23 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 def perform_query():
     cmd1 = request.args.get('cmd1')
     val1 = request.args.get('val1')
+    cmd2 = request.args.get('cmd2')
+    val2 = request.args.get('val2')
     file_name = request.args.get('file_name')
-    logging.info(f'Получен запрос с параметрами. cmd1 ={cmd1}, val1 = {val1}, file_name = {file_name}')
+    logging.info(f'Получен запрос с параметрами. cmd1 ={cmd1}, val1 = {val1}, '
+                 f'cmd2 ={cmd2}, val2 = {val2}, file_name = {file_name}')
     if not (cmd1 and val1 and file_name):
-        abort(400)
+        abort(400, 'Нет необходимого минимума команд!')
 
     file_path = os.path.join(DATA_DIR, file_name)
     if not os.path.exists(file_path):
         return abort(400, 'File not found')
 
     with open(file_path) as file:
-        if cmd1 == 'filter':
-            res = [x for x in file if val1 in x]
-
-        if cmd1 == 'map':
-            val = int(val1)
-            res = '\n'.join([x.split()[val] for x in file])
-            return res
-    # logging.info(f'Функция вернула {res}, с типом данных {type(res)}')
-
-        if cmd1 == 'unique':
-            res = list(set(file))
-    return jsonify(res)
+        result = get_query(cmd1, val1, file)
+        if cmd2 and val2:
+            result = get_query(cmd2, val2, result)
+        return jsonify(result)
 
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
---
+++ utils.py
@@ -0,0 +1,19 @@
+def get_query(cmd, val, file):
+    global res
+    if cmd == 'filter':
+        res = [x for x in file if val in x]
+
+    if cmd == 'map':
+        res = '\n'.join([x.split()[int(val)] for x in file])
+
+    if cmd == 'unique':
+        res = list(set(file))
+
+    if cmd == 'sort':
+        is_reverse = val == 'desc'
+        res = sorted(file, reverse=is_reverse)
+
+    if cmd == 'limit':
+        res = list(file)[:int(val)]
+
+    return res
